b6507a752d30d292e5dbc421bcf7e64d50ec0163
kyle <aizuguo_@126.com>
2019-07-31 12:31:12 +0800

    Update start_tomcat.sh

--- devops/start_tomcat.sh
+++ devops/start_tomcat.sh
@@ -5,10 +5,13 @@ INTERNAL_PORT=8080
 SHELL_DIR=$(dirname "$BASH_SOURCE")
 APP_DIR=$(cd $SHELL_DIR; pwd)
 
+cd $APP_DIR
+mvn clean package
+
 if [ "$(docker ps -aq -f name=$NAME)" ]; then
 	# stop and run
-	docker stop $NAME && docker rm $NAME && docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR/target:/usr/local/tomcat/webapps tomcat:alpine
+	docker stop $NAME && docker rm $NAME && docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR/target:/usr/local/tomcat/webapps:z tomcat:alpine
 else
 	# run
-	docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR/target:/usr/local/tomcat/webapps tomcat:alpine
+	docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR/target:/usr/local/tomcat/webapps:z tomcat:alpine
 fi
