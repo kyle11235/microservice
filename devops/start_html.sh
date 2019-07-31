a575e1ef327ad42a250c007529eb8a58c190672f
kyle <aizuguo_@126.com>
2019-07-31 12:31:53 +0800

    Update start_html.sh

--- devops/start_html.sh
+++ devops/start_html.sh
@@ -7,8 +7,8 @@ APP_DIR=$(cd $SHELL_DIR; pwd)
 
 if [ "$(docker ps -aq -f name=$NAME)" ]; then
 	# stop and run
-	docker stop $NAME && docker rm $NAME && docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR:/usr/share/nginx/html nginx
+	docker stop $NAME && docker rm $NAME && docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR:/usr/share/nginx/html:z nginx
 else
 	# run
-	docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR:/usr/share/nginx/html nginx
+	docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR:/usr/share/nginx/html:z nginx
 fi
