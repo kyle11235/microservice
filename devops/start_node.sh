cda110d7fcb21f8170d080492ae3c454a545d73a
kyle <aizuguo_@126.com>
2019-07-31 12:32:38 +0800

    Update start_node.sh

--- devops/start_node.sh
+++ devops/start_node.sh
@@ -7,8 +7,8 @@ APP_DIR=$(cd $SHELL_DIR; pwd)
 
 if [ "$(docker ps -aq -f name=$NAME)" ]; then
 	# stop and run
-	docker stop $NAME && docker rm $NAME && docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR:/opt/app node:6 /bin/sh -c "node /opt/app/index.js"
+	docker stop $NAME && docker rm $NAME && docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR:/opt/app:z node:6 /bin/sh -c "node /opt/app/index.js"
 else
 	# run
-	docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR/opt/app node:6 /bin/sh -c "node /opt/app/index.js"
+	docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR/opt/app:z node:6 /bin/sh -c "node /opt/app/index.js"
 fi
