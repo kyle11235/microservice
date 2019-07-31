f504c2785f9a2b4a8bb3db25149dc35bf4786524
kyle <aizuguo_@126.com>
2019-07-31 12:30:24 +0800

    Update start_java.sh

--- devops/start_java.sh
+++ devops/start_java.sh
@@ -1,3 +1,5 @@
+# requires target/app.jar
+
 NAME=java
 PORT=8080
 INTERNAL_PORT=8080
@@ -5,10 +7,13 @@ INTERNAL_PORT=8080
 SHELL_DIR=$(dirname "$BASH_SOURCE")
 APP_DIR=$(cd $SHELL_DIR; pwd)
 
+cd $APP_DIR
+mvn clean package
+
 if [ "$(docker ps -aq -f name=$NAME)" ]; then
 	# stop and run
-	docker stop $NAME && docker rm $NAME && docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR:/opt openjdk /bin/sh -c "java -jar /opt/app.jar"
+	docker stop $NAME && docker rm $NAME && docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR/target:/opt:Z openjdk /bin/sh -c "java -jar /opt/app.jar"
 else
 	# run
-	docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR:/opt openjdk /bin/sh -c "java -jar /opt/app.jar"
+	docker run --name $NAME -d -p $PORT:$INTERNAL_PORT -v $APP_DIR/target:/opt:z openjdk /bin/sh -c "java -jar /opt/app.jar"
 fi
