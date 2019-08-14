f772e83cc444b894fc229afa361ff28f0f86379d
kyle <aizuguo_@126.com>
2019-08-14 14:38:58 +0800

    Update ssh.sh

--- devops/ssh.sh
+++ devops/ssh.sh
@@ -2,4 +2,4 @@ pwd
 ls
 chmod 400 id_rsa
 # reset to avoid pull merge issue
-ssh -o StrictHostKeyChecking=no -i id_rsa opc@x.x.x.x 'cd /home/opc/apps && git reset --hard && git pull && bash -s < deploy.sh'
+ssh -o StrictHostKeyChecking=no -i id_rsa opc@x.x.x.x 'cd /home/opc/apps && bash -s < reset.sh && bash -s < update.sh && bash -s < deploy.sh'
