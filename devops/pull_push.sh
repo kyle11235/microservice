598ccf7b2f5af2195a71b934b417b9cfddc73562
kyle <aizuguo_@126.com>
2019-08-25 01:09:16 +0800

    Update pull_push.sh

--- devops/pull_push.sh
+++ devops/pull_push.sh
@@ -1,3 +1,15 @@
+# path
+SHELL_DIR=$(dirname "$BASH_SOURCE")
+APP_DIR=$(cd $SHELL_DIR; pwd)
+cd $APP_DIR
+
+# comment
+COMMENT="backup"
+if [ ! -z "$1" ]
+  then
+    COMMENT=$1
+fi
+
 # merge without editing comment
 git pull --no-edit
 
@@ -6,5 +18,5 @@ git add *
 # stage tracked files including delete
 git add -u
 
-git commit -m "backup"
+git commit -m $COMMENT
 git push
